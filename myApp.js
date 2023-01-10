5c11fe821159574e5c937824a2cf8616a17da5b4
simeonbikov <bikovsem@gmail.com>
2023-01-10 12:53:07 +0000

    added middleware

--- myApp.js
+++ myApp.js
@@ -1,17 +1,11 @@
 let express = require('express');
 let app = express();
 
-// app.get("/", function (req, res) {
-//   res.send("Hello Express");
-// });
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-
-// console.log("Hello World");
-
-
 
 
 
