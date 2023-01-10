6b8f29931b760af57c8b5859f8c38ef3d00a3975
simeonbikov <bikovsem@gmail.com>
2023-01-10 17:07:55 +0000

    chain middleware / time server

--- myApp.js
+++ myApp.js
@@ -5,9 +5,9 @@ require("dotenv").config();
 let app = express();
 
 app.use((req, res, next) => {
-    const str = `${req.method} ${req.path} - ${req.ip}`;
-    console.log(str);
-    next();
+  const str = `${req.method} ${req.path} - ${req.ip}`;
+  console.log(str);
+  next();
 });
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -17,14 +17,23 @@ app.get("/", function (req, res) {
 });
 
 app.get("/json", (req, res) => {
-  let msg = {
-    message: "Hello json",
-  };
-
+  let msg = { message: "Hello json" };
   if (process.env.MESSAGE_STYLE === "uppercase") {
     msg.message = msg.message.toUpperCase();
   }
-    res.json(msg);
+  res.json(msg);
 });
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    const curTime = { time: req.time };
+    res.json({ curTime });
+  }
+);
 
 module.exports = app;
