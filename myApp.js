18b7874b893e44457314fd9c13dddbc111737eeb
simeonbikov <bikovsem@gmail.com>
2023-01-10 13:01:06 +0000

    served json

--- myApp.js
+++ myApp.js
@@ -7,6 +7,12 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
+
 
 
 
